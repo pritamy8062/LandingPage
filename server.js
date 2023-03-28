2b3e4a250a7bbe6a058f698ff57f9eb0be730ae8
pritamy8062 <120549375+pritamy8062@users.noreply.github.com>
2023-03-29 02:10:33 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+import path from "path";
+
+import noteRoutes from "./routes/noteRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express(); // main thing
+
+app.use(express.json()); // to accept json data
+
+app.use("/api/notes", noteRoutes);
+app.use("/api/users", userRoutes);
+
+// --------------------------deployment------------------------------
+const __dirname = path.resolve();
+
+
+
+
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+
+// --------------------------deployment------------------------------
+
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+);
